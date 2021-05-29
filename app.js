e8b088b6788e1fd9e29160e5af72281b5c8f0d4f
Cigogma <razvangarboveanu@gmail.com>
2021-05-30 01:13:47 +0300

    latest changes

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function (socket) {
-  console.log('a user connected');
-  socket.on('command', function (data) {
-    io.emit('exec', { command: data });
+io.on("connection", function (socket) {
+  console.log("sonda connected");
+  socket.on("command", function (data) {
+    io.emit("exec", { command: data });
   });
-  socket.on('log', function (data) {
-    io.emit('log', data);
+  socket.on("log", function (data) {
+    console.log(data);
+    io.emit("log", data);
   });
+});
+
+io.on("disconnect", function (data) {
+  console.log("sonda disconnected");
 });
 
 http.listen(3000, function () {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
